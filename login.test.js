ea4d3fb051fbc19427eb0b2442429dddf31e71b3
BinduMadhaviTammireddy <tammireddymadhavi@gmail.com>
2019-11-16 16:19:19 +1100

    input details

--- login.test.js
+++ login.test.js
@@ -8,7 +8,7 @@ describe('Login Functionality Testing', () => {
     beforeAll(async () => {
         browser = await puppeteer.launch({
             headless: false,
-            slowMo: 80,
+            slowMo: 250,
             args: [`--window-size=${width},${height}`]
         });
         page = await browser.newPage();
@@ -19,13 +19,25 @@ describe('Login Functionality Testing', () => {
       
   
     it('Login and Logout', async () => {
-        jest.setTimeout(10000);
+        jest.setTimeout(15000);
         await page.mouse.click(0, 100);
-        await page.waitFor(2000);
+        await page.waitFor(3000);
       // click on login using selector.
       await page.click('#root > div > div.sc-bdVaJa.bFlryC > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(1) > button:nth-child(1) > span.MuiButton-label');
-    //
-        await page.waitFor(4000);
+    // input username
+    //await page.focus('body > div.MuiDialog-root > div.MuiDialog-container.MuiDialog-scrollPaper > div');
+    var username_input = await page.evaluate(()=>
+    {
+    (await page.$x("//input[contains(text(), 'E-mail')]")[1];
+    });
+    await page.$eval(username_input, 'test@gmail.com');
+   //await page.type('[value name^="E-mail"]', 'test@gmail.com', {delay: 200}) ;
+    //await page.$eval('input[type=text]', el => el.value = 'test@gmail.com');
+    await page.$eval('input[type=password]', el => el.value = 'test1234');
+ 
+     // click on submit button
+    await page.click('button', { text: 'Submit' });
+    await page.waitFor(3000);
     });
     afterAll(() => {
         browser.close();

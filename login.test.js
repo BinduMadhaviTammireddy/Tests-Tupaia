aa8b7375507904ff4b3c48acdff11a594f174e58
BinduMadhaviTammireddy <tammireddymadhavi@gmail.com>
2019-11-16 21:26:39 +1100

    input details

--- login.test.js
+++ login.test.js
@@ -24,15 +24,9 @@ describe('Login Functionality Testing', () => {
         await page.waitFor(3000);
       // click on login using selector.
       await page.click('#root > div > div.sc-bdVaJa.bFlryC > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(1) > button:nth-child(1) > span.MuiButton-label');
+        await page.waitFor(1000);
     // input username
-    //await page.focus('body > div.MuiDialog-root > div.MuiDialog-container.MuiDialog-scrollPaper > div');
-    var username_input = await page.evaluate(()=>
-    {
-    (await page.$x("//input[contains(text(), 'E-mail')]")[1];
-    });
-    await page.$eval(username_input, 'test@gmail.com');
-   //await page.type('[value name^="E-mail"]', 'test@gmail.com', {delay: 200}) ;
-    //await page.$eval('input[type=text]', el => el.value = 'test@gmail.com');
+  
     await page.$eval('input[type=password]', el => el.value = 'test1234');
  
      // click on submit button

9129d1b176854e175861fff62818205f5007be16
BinduMadhaviTammireddy <tammireddymadhavi@gmail.com>
2019-11-17 20:35:03 +1100

    Forget Password and Readme.md

---
+++ README.md
@@ -0,0 +1,23 @@
+## Tupaia Login Functionality Test Cases
+
+Steps
+
+```
+clone repo
+```
+```
+npm install
+```
+```
+signup using credentials in .env file or update .env file accordingly
+```
+To Run Tests
+
+```
+npm test
+```
+
+### Forget Password Test Case
+```
+Check Email for Reset Password Link if received test case successful else fail
+```
--- forgetPassword.test.js
+++ forgetPassword.test.js
@@ -5,7 +5,7 @@ let browser;
 const width = 1500;
 const height = 1000;
 jest.setTimeout(60000);
-describe('Login Functionality Testing', () => {
+describe('Forgot password Functionality Testing', () => {
     beforeAll(async () => {
         browser = await puppeteer.launch({
             headless: false,
@@ -18,20 +18,15 @@ describe('Login Functionality Testing', () => {
     });
       
   
-    it('Login and checking username', async () => {
+    it('Forget Password checking', async () => {
         await page.click('body > div.MuiDialog-root > div.MuiDialog-container.MuiDialog-scrollPaper > div > div > svg')
         await page.$eval('button[class="MuiButtonBase-root MuiButton-root MuiButton-text"]', elem => elem.click());
-        await page.waitForSelector('div[class="MuiDialogContent-root"] input[type="text"]');
-        await page.type('div[class="MuiDialogContent-root"] input[type="text"]',process.env.User1_Username);
-        await page.waitForSelector('input[type=password]');
-        await page.type('input[type=password]',process.env.User1_Password);
-        await page.waitForSelector('div[class="MuiDialogContent-root"] button[type="button"]');
+        const aElementsWithHi = await page.$x("//a[contains(., 'Forgot your password?')]");
+        await aElementsWithHi[0].click();
+        await page.waitFor(2000);
+        await page.waitForSelector('div[class="MuiDialogContent-root"] input[type="email"]');
+        await page.type('div[class="MuiDialogContent-root"] input[type="email"]',process.env.User1_Username);
         await page.click('div[class="MuiDialogContent-root"] button[type="button"]');
-        await page.waitForSelector('button[class="MuiButtonBase-root MuiButton-root MuiButton-text"]');
-        const element = await page.$(".MuiButton-label");
-        const text = await page.evaluate(element => element.textContent, element);
-        await expect(text).toMatch(process.env.UserName_1);
-
         });
     afterAll(() => {
         browser.close();
